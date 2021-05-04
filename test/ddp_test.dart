c905c8cf27396e0c9cb41efb14013d277c637cb3
maryan <genzhalo@gmail.com>
2021-05-04 19:09:29 +0300

    Migrate to nullsafety

--- test/ddp_test.dart
+++ test/ddp_test.dart
@@ -6,9 +6,11 @@ void main() {
     print(16.toRadixString(16));
   });
 
-  test('DdpClientTest', () {
-    DdpClient client = DdpClient(
-        'DdpClientTest', 'ws://localhost:3000/websocket', 'http://localhost');
-    client.connect();
-  });
+
+  final client = DdpClient('DdpClientTest', 'ws://localhost:3000/websocket', 'http://localhost');
+  test('DdpClientTest', ()  async {
+   
+    await client.connect();
+    await Future.delayed(Duration(minutes: 2));
+  }, timeout: Timeout(Duration(minutes: 2)));
 }
